cca6a80cc39ef2184d0f5b97cfa1f25f43e93c56
Eren Budur <erenbudur33@gmail.com>
2022-10-04 17:17:41 -0400

    created verticle fragment

--- W4_P5/app/src/main/java/com/example/w4_p5/MainActivity.java
+++ W4_P5/app/src/main/java/com/example/w4_p5/MainActivity.java
@@ -2,13 +2,22 @@ package com.example.w4_p5;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import model.Hangman;
 
+public class MainActivity extends AppCompatActivity {
+    private Hangman hangman;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if(hangman == null){
+            hangman = new Hangman(Hangman.DEFAULT_GUESSES);
+        }
         setContentView(R.layout.activity_main);
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction( );
     }
 }
